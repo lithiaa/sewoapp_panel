63fca489cc4245bac4da0b31e1014e1c1d3cfd5d
Muhammad Bagus Indrawan <100072786+lithiaa@users.noreply.github.com>
2025-10-14 21:38:18 +0700

    feat: add categories for vehicle types in database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Category;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -19,6 +20,19 @@ class DatabaseSeeder extends Seeder
             'email' => 'admin@admin.com',
             'phone_number' => '08234567890',
             'address' => 'Jl. Dinoyo Permai, lowokwaru, Kota Malang'
+        ]);
+
+        Category::create([
+            'name' => 'SeMobil',
+            'desc' => 'Kategori kendaraan roda empat (mobil) yang tersedia di Sewoapp. Kategori ini mencakup berbagai jenis mobil seperti sedan, SUV, hatchback, dan lainnya, yang dapat disewa oleh pengguna untuk keperluan pribadi atau bisnis.',
+        ]);
+        Category::create([
+            'name' => 'SeMotor',
+            'desc' => 'Kategori kendaraan roda dua (motor) yang tersedia di Sewoapp. Kategori ini mencakup berbagai jenis motor seperti skuter, bebek, sport, dan lainnya, yang dapat disewa oleh pengguna untuk keperluan sehari-hari atau perjalanan singkat.',
+        ]);
+        Category::create([
+            'name' => 'SeMolis',
+            'desc' => 'Kategori kendaraan mobil/motor listrik (Molis) yang tersedia di Sewoapp. Kategori ini mencakup berbagai jenis kendaraan listrik yang ramah lingkungan, yang dapat disewa oleh pengguna untuk keperluan pribadi atau bisnis, sambil mendukung gaya hidup berkelanjutan.',
         ]);
 
 
